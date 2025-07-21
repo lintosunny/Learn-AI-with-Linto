5e5b7664cdf264fda423332c7d13d2c8dd31897a
lintosunny <lintosunny111@gmail.com>
2025-07-21 19:37:33 +0530

    python exercise

---
+++ week_01_python/exercises/03_fizz_buzz_sum.py
@@ -0,0 +1,11 @@
+# credits: datalemur.com
+# Write a function fizz_buzz_sum to find the sum of all multiples of 3 or 5 below a target value.
+# For example, if the target value was 10, the multiples of 3 or 5 below 10 are 3, 5, 6, and 9.
+# 3 + 5 + 6 + 9 = 23
+
+def fizz_buzz_sum(target):
+  sum = 0
+  for i in range(target):
+    if (i % 3 == 0) or (i % 5 == 0):
+      sum += i
+  return sum
